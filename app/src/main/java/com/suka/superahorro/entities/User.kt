38da326d45eead594f1622c5c126a423ef98d37d
Andres Esteban Sukanec <andressukanec@hotmail.com>
2023-05-09 20:15:43 -0300

    added navgraphs

--- app/src/main/java/com/suka/superahorro/entities/User.kt
+++ app/src/main/java/com/suka/superahorro/entities/User.kt
@@ -1,4 +1,27 @@
 package com.suka.superahorro.entities
 
-class User {
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "users")
+class User (mail : String, pass : String) {
+
+    @PrimaryKey()
+    @ColumnInfo(name = "mail")
+    var mail: String = mail
+
+    @ColumnInfo(name = "pass")
+    var pass: String = pass
+
+    @ColumnInfo(name = "firstname")
+    var firstname: String? = null
+
+    @ColumnInfo(name = "lastname")
+    var lastname: String? = null
+
+    @ColumnInfo(name = "picture")
+    var picture: String? = null
 }
