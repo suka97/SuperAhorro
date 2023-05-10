14123de39e9486e6e7a8978aa44390ba5d862959
Andres Esteban Sukanec <andressukanec@hotmail.com>
2023-05-09 22:50:47 -0300

    testeado login, ver imagenes en recycler view que no se ven

--- app/src/main/java/com/suka/superahorro/fragments/LoginFragment.kt
+++ app/src/main/java/com/suka/superahorro/fragments/LoginFragment.kt
@@ -1,32 +1,93 @@
 package com.suka.superahorro.fragments
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.suka.superahorro.R
+import com.suka.superahorro.activities.MainActivity
+import com.suka.superahorro.database.AppDatabase
+import com.suka.superahorro.database.UserDao
 
 class LoginFragment : Fragment() {
+    lateinit var v : View
 
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
+    private var db: AppDatabase? = null
+    private var usersDao: UserDao? = null
+    lateinit var sharedPreferences: SharedPreferences
 
-    private lateinit var viewModel: LoginViewModel
+    lateinit var btLogin : Button
+    lateinit var btSignup : Button
+    lateinit var txtEmail : EditText
+    lateinit var txtPass : EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        v = inflater.inflate(R.layout.fragment_login, container, false)
+
+        db = AppDatabase.getInstance(v.context)
+        usersDao = db?.userDao()
+        sharedPreferences = requireContext().getSharedPreferences("com.suka.superahorro.PREFERENCES", Context.MODE_PRIVATE)
+
+        btLogin = v.findViewById(R.id.btLogin)
+        btSignup = v.findViewById(R.id.btSignup)
+        txtEmail = v.findViewById(R.id.txtEmail_login)
+        txtPass = v.findViewById(R.id.txtPass_login)
+
+        return v
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+
+    fun login() {
+        startActivity(Intent(context, MainActivity::class.java))
+        requireActivity().finish()
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        val prefMail: String = sharedPreferences.getString("user_mail", "") ?:""
+        val prefPass: String = sharedPreferences.getString("user_pass", "") ?:""
+        val prefUser = usersDao?.fetchUserByCredentials(prefMail, prefPass)
+        if ( prefUser != null ) login()
+
+        btLogin.setOnClickListener() {
+            val userMail = txtEmail.text.toString()
+            val userPass = txtPass.text.toString()
+
+            if ( userMail.isEmpty() || userPass.isEmpty() ) {
+                Snackbar.make(v, "Debe completar todos los campos", Snackbar.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val user = usersDao?.fetchUserByCredentials(userMail, userPass)
+            if ( user == null ) {
+                Snackbar.make(v, "Usuario o contraseña incorrectos", Snackbar.LENGTH_SHORT).show()
+            }
+            else {
+                val prefEditor = sharedPreferences.edit()
+                prefEditor.putString("user_mail", userMail)
+                prefEditor.putString("user_pass", userPass)
+                prefEditor.apply()
+
+                login()
+            }
+        }
+        btSignup.setOnClickListener() {
+            val action = LoginFragmentDirections.actionLoginFragmentToSignupFragment()
+            findNavController().navigate(action)
+        }
     }
 
 }
