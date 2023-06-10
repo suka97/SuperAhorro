dbd1035f1d5e59270310f733de76a24ac1e531ec
Andres Esteban Sukanec <andressukanec@hotmail.com>
2023-06-10 01:51:53 -0300

    firebase first auth

--- app/src/main/java/com/suka/superahorro/fragments/CartViewModel.kt
+++ app/src/main/java/com/suka/superahorro/fragments/CartViewModel.kt
@@ -1,13 +1,18 @@
 package com.suka.superahorro.fragments
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.suka.superahorro.database.AppDatabase
 import com.suka.superahorro.database.CartItemDao
 import com.suka.superahorro.entities.CartItem
 
 class CartViewModel : ViewModel() {
     private lateinit var context: Context
+    private lateinit var auth: FirebaseAuth
 
     private var db: AppDatabase? = null
     private var cartItemDao: CartItemDao? = null
@@ -21,6 +26,17 @@ class CartViewModel : ViewModel() {
         db = AppDatabase.getInstance(context)
         cartItemDao = db?.cartItemDao()
         cartItems = cartItemDao?.fetchAllCartItems() ?: mutableListOf<CartItem>()
+
+        auth = Firebase.auth
+        auth.signInWithEmailAndPassword("user@mail.com", "pass123")
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    Log.d("firebase", "signInWithEmail:success")
+                } else {
+                    Log.d("firebase", "signInWithEmail:failure")
+                }
+            }
     }
 
 
