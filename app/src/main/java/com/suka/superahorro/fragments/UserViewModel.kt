5481b04c8c519891034d1b4a84adebce2e36bfeb
Andres Esteban Sukanec <andressukanec@hotmail.com>
2023-06-10 19:22:02 -0300

    testeado el logeo y deslogeo

--- app/src/main/java/com/suka/superahorro/fragments/UserViewModel.kt
+++ app/src/main/java/com/suka/superahorro/fragments/UserViewModel.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.suka.superahorro.database.AppDatabase
 import com.suka.superahorro.database.CartItemDao
 import com.suka.superahorro.database.UserDao
@@ -13,38 +15,29 @@ import com.suka.superahorro.entities.User
 class UserViewModel : ViewModel() {
     private lateinit var context: Context
 
-    private var db: AppDatabase? = null
-    private var usersDao: UserDao? = null
-    lateinit var sharedPreferences: SharedPreferences
-
     var user = MutableLiveData<User>()
 
 
     fun init(context: Context) {
         this.context = context
-        db = AppDatabase.getInstance(context)
-        usersDao = db?.userDao()
-        sharedPreferences = context.getSharedPreferences("com.suka.superahorro.PREFERENCES", Context.MODE_PRIVATE)
 
-        val mail: String = sharedPreferences.getString("user_mail", "") ?:""
-        val pass: String = sharedPreferences.getString("user_pass", "") ?:""
-        user.value = usersDao?.fetchUserByCredentials(mail, pass)
+        val mail: String = Firebase.auth.currentUser?.email ?:""
+//        val pass: String = Firebase.auth.currentUser?.uid ?:""
+        user.value = User(mail, "")
     }
 
 
     fun clearLoginInfo() {
-        sharedPreferences.edit().clear().apply()
+        Firebase.auth.signOut()
     }
 
 
     fun getUser() : User {
-        return user.value!!
+        return User("","")
     }
 
 
     fun updateUser(newUser: User) {
-        user.value = newUser
-        usersDao?.updateUser(user.value!!)
-        sharedPreferences.edit().putString("user_pass", newUser.pass).apply()
+
     }
 }
