33f5a7d6268b80e42d389b1b0f38d0f349033d8d
Andres Esteban Sukanec <andressukanec@hotmail.com>
2023-06-24 20:44:38 -0300

    FUNCIONANDO uso NabigationDrawer

--- app/src/main/java/com/suka/superahorro/activities/MainActivity.kt
+++ app/src/main/java/com/suka/superahorro/activities/MainActivity.kt
@@ -4,23 +4,56 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.suka.superahorro.R
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bottomNavView : BottomNavigationView
-    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var navView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
-        bottomNavView = findViewById(R.id.bottomNavigationView)
+        navView = findViewById(R.id.nav_view)
+        drawerLayout = findViewById(R.id.drawer_layout)
 
-        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
+        setupSimpleNavigation()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return if (::appBarConfiguration.isInitialized) {
+            NavigationUI.navigateUp(navController, appBarConfiguration)
+        } else {
+            NavigationUI.navigateUp(navController, drawerLayout)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun setupSimpleNavigation() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
     }
 }
