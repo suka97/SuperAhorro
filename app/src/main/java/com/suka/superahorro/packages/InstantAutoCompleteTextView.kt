941146ec2264e857603dd16efe71836aff260acc
Andres Sukanec <andressukanec@hotmail.com>
2023-07-19 12:01:18 -0300

    fixes instant auto complete textview

--- app/src/main/java/com/suka/superahorro/packages/InstantAutoCompleteTextView.kt
+++ app/src/main/java/com/suka/superahorro/packages/InstantAutoCompleteTextView.kt
@@ -2,6 +2,7 @@ package com.suka.superahorro.packages
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.KeyEvent
 import android.widget.AutoCompleteTextView
 import androidx.appcompat.widget.AppCompatAutoCompleteTextView
 
@@ -15,5 +16,22 @@ class InstantAutoCompleteTextView @JvmOverloads constructor(
     // Mostrar sugerencias tan pronto como se empiece a escribir
     override fun enoughToFilter(): Boolean {
         return true
+    }
+
+
+    // mostrar lista on focus
+    override fun onFocusChanged(focused: Boolean, direction: Int, previouslyFocusedRect: android.graphics.Rect?) {
+        super.onFocusChanged(focused, direction, previouslyFocusedRect)
+        if (focused) {
+            // Mostrar sugerencias cuando el TextView obtenga el foco
+            showDropDown()
+        }
+    }
+
+
+    // clear focus on back pressed
+    override fun onKeyPreIme(keyCode: Int, event: KeyEvent?): Boolean {
+        clearFocus()
+        return super.onKeyPreIme(keyCode, event)
     }
 }
